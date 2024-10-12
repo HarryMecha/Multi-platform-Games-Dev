d439511eef73d6a9a27b2b5fc4ee698ce34ec821
HarryMecha <harrymecha@gmail.com>
2024-10-12 18:41:02 +0100

    Added in movement functionality
    I added in the Unity InputSystem package, then adapted the PlayerController code to accommodate for it, WASD movement and updated the character looking script.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,45 +1,92 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
 
     public Camera PlayerCamera;
-    public float CameraSensitivity;
-    private float rotationX;
-    private float rotationY;
+    public float cameraSensitivity;
     public float HitDistance;
+    private Rigidbody rigidbody;
+    private string currentGroundType;
+    public Vector2 moveValue;
+    public float moveSpeed;
+    public Vector2 lookValue;
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private Vector3 cameraRotation;
 
-    private void Start()
+    private void Awake()
     {
+        rigidbody = GetComponent<Rigidbody>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-        rotationX = 0f; 
-        rotationY = 0f;
+        currentGroundType = "Jumping";
     }
 
-    // Update is called once per frame
+    public void OnMove(InputValue value)
+    {
+        moveValue = value.Get<Vector2>();
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookValue = value.Get<Vector2>();
+    }
+
+    void OnFire(InputValue value)
+    {
+        Ray ray = PlayerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, HitDistance))
+        {
+            Debug.Log("You are looking at " + hit.transform.name);
+        }
+
+    }
+    void OnJump(InputValue value)
+    {
+        Debug.Log("Jump");
+    }
+    
+
+    private void FixedUpdate()
+    {
+        Vector2 moveDirection = moveValue;
+        Vector3 velocity = rigidbody.velocity;
+        velocity.x = moveSpeed * moveDirection.x;
+        velocity.z = moveSpeed * moveDirection.y;
+        rigidbody.velocity = velocity;
+    }
+
     void Update()
     {
+        cameraRotation.x += lookValue.y * cameraSensitivity;
+        cameraRotation.y += lookValue.x * cameraSensitivity;
+        cameraRotation.x = Mathf.Clamp(cameraRotation.x, -90f, 90f);
+        PlayerCamera.transform.localRotation = Quaternion.Euler(-cameraRotation.x, 0f, 0f); // Camera rotation
+        transform.localRotation = Quaternion.Euler(0f, cameraRotation.y, 0f); // Player rotation
+    }
 
-        rotationX += Input.GetAxis("Mouse Y") * CameraSensitivity;
-        rotationX = Mathf.Clamp(rotationX, -90, 90);
-        rotationY += Input.GetAxis("Mouse X") * -1 * CameraSensitivity;
-        PlayerCamera.transform.localEulerAngles = new Vector3 (rotationX, rotationY, 0f);
-
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
         {
-            Ray ray = PlayerCamera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if(Physics.Raycast(ray, out hit, HitDistance))
-            {
-                Debug.Log("You are looking at " + hit.transform.name);
-            }
-            
+            currentGroundType = collision.gameObject.name;
         }
     }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            currentGroundType = "Jumping";
+        }
+    }
+
 }
